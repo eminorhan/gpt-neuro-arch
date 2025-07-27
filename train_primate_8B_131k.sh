6e74ab9b51bb71fb972f32607aa9e5ea654314a2
Emin Orhan <aeminorhan@gmail.com>
2025-07-27 22:18:13 +0000

    add aws-ofi-nccl

--- train_primate_8B_131k.sh
+++ train_primate_8B_131k.sh
@@ -9,15 +9,20 @@
 #SBATCH --time=6:00:00
 #SBATCH --job-name=train_primate_8b_131k
 #SBATCH --output=train_primate_8b_131k_%A_%a.out
-#SBATCH --array=0-15
+#SBATCH --array=0-9
 
 # activate venv
 # source /lustre/gale/stf218/scratch/emin/myvenv/bin/activate
 eval "$(/lustre/gale/stf218/scratch/emin/container/miniconda3/bin/conda shell.bash hook)"
 
 # set misc env vars
+export LD_LIBRARY_PATH=/lustre/gale/stf218/scratch/emin/aws-ofi-nccl-1.14.0/lib:$LD_LIBRARY_PATH  # enable aws-ofi-nccl
+export NCCL_NET=ofi
+export FI_PROVIDER=cxi
 export LOGLEVEL=INFO
 export OMP_NUM_THREADS=1
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+export GLOO_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
 export NCCL_NET_GDR_LEVEL=3   # can improve performance, but remove this setting if you encounter a hang/crash.
 export NCCL_CROSS_NIC=1       # on large systems, this nccl setting has been found to improve performance
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
