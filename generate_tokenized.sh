7dc1210fb32770d17440fb9134fbd0e627a05dc6
Emin Orhan <aeminorhan@gmail.com>
2026-01-20 00:28:40 +0000

    misc updates

--- generate_tokenized.sh
+++ generate_tokenized.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=288
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=4
-#SBATCH --time=00:10:00
+#SBATCH --time=00:11:00
 #SBATCH --job-name=generate_tokenized
 #SBATCH --output=generate_tokenized_%A_%a.out
 #SBATCH --array=0
@@ -15,15 +15,8 @@
 source /lustre/blizzard/stf218/scratch/emin/blizzardvenv/bin/activate
 
 # set misc env vars
-# export LD_LIBRARY_PATH=/lustre/blizzard/stf218/scratch/emin/aws-ofi-nccl-1.17.2/lib:$LD_LIBRARY_PATH  # enable aws-ofi-nccl
-# export NCCL_NET=ofi
-# export FI_PROVIDER=cxi
 export LOGLEVEL=INFO
 export OMP_NUM_THREADS=1
-# export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
-# export GLOO_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
-# export NCCL_NET_GDR_LEVEL=3   # can improve performance, but remove this setting if you encounter a hang/crash.
-# export NCCL_CROSS_NIC=1       # on large systems, this nccl setting has been found to improve performance
 export HF_HOME="/lustre/blizzard/stf218/scratch/emin/huggingface"
 export HF_DATASETS_CACHE="/lustre/blizzard/stf218/scratch/emin/huggingface"
 export TRITON_CACHE_DIR="/lustre/blizzard/stf218/scratch/emin/triton"
@@ -38,6 +31,6 @@ export MASTER_PORT=3442
 
 CONFIG_FILE=${CONFIG_FILE:-"./train_configs/primate_7B_8k_n_fixed_256_tokenizer_1x15_32k.toml"}
 
-srun torchrun --nnodes $SLURM_NNODES --nproc_per_node 4 --max_restarts 1 --node_rank $SLURM_NODEID --rdzv_id 101 --rdzv_backend c10d --rdzv_endpoint "$MASTER_ADDR:$MASTER_PORT" ./generate_tokenized.py --config ${CONFIG_FILE} --tokenizer_path "tokenizers/tokenizer_primate_1x15_32k.pkl" --ckpt "outputs/primate_7B_8k_n_fixed_256_tokenizer_1x15_32k/checkpoint/step-91800"
+srun torchrun --nnodes $SLURM_NNODES --nproc_per_node 4 --max_restarts 1 --node_rank $SLURM_NODEID --rdzv_id 101 --rdzv_backend c10d --rdzv_endpoint "$MASTER_ADDR:$MASTER_PORT" ./generate_tokenized.py --config ${CONFIG_FILE} --tokenizer_path "tokenizers/tokenizer_primate_1x15_32k.pkl" --ckpt "outputs/primate_7B_8k_n_fixed_256_tokenizer_1x15_32k/checkpoint/step-99000"
 
 echo "Done"
