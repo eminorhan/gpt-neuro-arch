a7933c79a53dd82c98980ba60ab3db6e7f960315
Emin Orhan <aeminorhan@gmail.com>
2026-02-01 21:41:02 +0000

    remove n8

--- download_dataset.py
+++ download_dataset.py
@@ -3,9 +3,9 @@ import aiohttp
 import numpy as np
 import torch
 
-ds = load_dataset("eminorhan/neural-pile-primate-reordered", download_mode='force_redownload')
+# ds = load_dataset("eminorhan/neural-pile-primate-reordered", download_mode='force_redownload')
 # ds = load_dataset("eminorhan/neural-pile-rodent-reordered", download_mode='force_redownload')
-# ds = load_dataset("eminorhan/neural-pile-primate", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
+ds = load_dataset("eminorhan/neural-pile-primate", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
 # ds = load_dataset("eminorhan/neural-pile-rodent", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
 # ds = load_dataset("eminorhan/xiao", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
 # ds = load_dataset("eminorhan/willett", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
@@ -19,11 +19,12 @@ ds = load_dataset("eminorhan/neural-pile-primate-reordered", download_mode='forc
 # ds = load_dataset("eminorhan/h2", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
 # ds = load_dataset("eminorhan/xiao_1ms", storage_options={'client_kwargs': {'timeout': aiohttp.ClientTimeout(total=3600)}})
 
-print(f"Number of data rows: {len(ds['train'])}")
-sp = np.array(ds['train'][0]['spike_counts'])
-print(f"sp: {sp}")
-print(f"Shape / unique: {sp.shape}/{np.unique(sp)}")
-# for d in ds["train"]:
-#     spike_counts = np.array(d['spike_counts'], dtype=np.uint8)
-#     print(f"Data row shape: {spike_counts.shape}")
+print(f"Number of data rows: {len(ds['test'])}")
+# sp = np.array(ds['train'][0]['spike_counts'])
+# print(f"sp: {sp}")
+# print(f"Shape / unique: {sp.shape}/{np.unique(sp)}")
+for d in ds["test"]:
+    # spike_counts = np.array(d['spike_counts'], dtype=np.uint8)
+    # print(f"Data row shape: {spike_counts.shape}")
+    print(f"Source dataset: {d['source_dataset']}")
 print(f"Done!")
